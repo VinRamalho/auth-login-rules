257355ccf344d1154bd9d255ca66a0608221ffff
VinRamalho <vinramalho10@gmail.com>
2024-02-04 22:39:00 -0300

    new layout

--- front/src/components/layout/index.tsx
+++ front/src/components/layout/index.tsx
@@ -1,18 +1,97 @@
+"use client";
+
 import "./style.css";
-import { ReactNode } from "react";
-import NavBar from "./navbar";
-import Header from "./header";
+import React, { ReactNode, useEffect, useState } from "react";
+import {
+  HomeOutlined,
+  LogoutOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button } from "antd";
+import { AuthService } from "@/service/auth";
+import { usePathname } from "next/navigation";
+import RedirectPage from "../redirect";
+
+const { Header, Sider, Content } = Layout;
 
 interface IProps {
   children: ReactNode;
 }
 
 export default function LayoutPage({ children }: IProps) {
+  const [collapsed, setCollapsed] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [redirect, setRedirect] = useState<string | undefined>(undefined);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const auth = AuthService.IsAuthenticated;
+
+    if (!auth && pathname !== "/login") {
+      setRedirect("/login");
+    }
+
+    setIsAuthenticated(auth);
+  }, []);
+
+  function onSignOut() {
+    AuthService.signOut();
+    window.location.reload();
+  }
+
+  if (!isAuthenticated) {
+    return <div className="div-login">{children}</div>;
+  }
+
+  if (redirect) {
+    return <RedirectPage url={redirect} />;
+  }
+
   return (
-    <>
-      <Header />
-      <NavBar />
-      <main>{children}</main>
-    </>
+    <Layout>
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className="navbar"
+      >
+        <div className="demo-logo-vertical" />
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <HomeOutlined />,
+              label: "Home",
+            },
+            {
+              key: "99",
+              icon: <LogoutOutlined />,
+              label: "Sair",
+              onClick: onSignOut,
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header>
+          <div>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              className="btn-collapse"
+            />
+
+            <span className="header-name">
+              Seja bem vindo, {AuthService.Name ?? "- "}!
+            </span>
+          </div>
+        </Header>
+        <Content className="layout-content">{children}</Content>
+      </Layout>
+    </Layout>
   );
 }
